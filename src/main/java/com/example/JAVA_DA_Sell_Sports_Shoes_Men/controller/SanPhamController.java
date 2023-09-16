6871c2f094c7519eba5e67c41d7f34b369499ecc
huyenhtt02 <huyenhttph25539@fpt.edu.vn>
2023-09-16 18:05:48 +0700

    startup

--- src/main/java/com/example/JAVA_DA_Sell_Sports_Shoes_Men/controller/SanPhamController.java
+++ src/main/java/com/example/JAVA_DA_Sell_Sports_Shoes_Men/controller/SanPhamController.java
@@ -1,4 +1,16 @@
 package com.example.JAVA_DA_Sell_Sports_Shoes_Men.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
 public class SanPhamController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello everyone!, Have a nice day !";
+    }
 }
