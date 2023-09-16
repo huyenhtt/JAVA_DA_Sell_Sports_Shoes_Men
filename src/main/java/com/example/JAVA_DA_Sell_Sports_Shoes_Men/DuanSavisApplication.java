8b0dd1729e795f9cb4dfd2a26e22a921d7bac99c
huyenhtt02 <huyenhttph25539@fpt.edu.vn>
2023-09-16 17:33:24 +0700

    one-push

---
+++ src/main/java/com/example/JAVA_DA_Sell_Sports_Shoes_Men/DuanSavisApplication.java
@@ -0,0 +1,13 @@
+package com.example.JAVA_DA_Sell_Sports_Shoes_Men;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuanSavisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuanSavisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JAVA_DA_Sell_Sports_Shoes_Men/DuanSavisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JAVA_DA_Sell_Sports_Shoes_Men;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuanSavisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
